f217cbc2ab36f8591b3cf0c0f34152caacc9d43a
Simon Inns <simoninns@fastmail.com>
2026-03-31 08:06:23 +0200

    Move to nix 25.11 stable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "encode-orc - LaserDisc and tape video encoder";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
